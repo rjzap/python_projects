fcd7636cb4c5d76793e0ee028e198c946a9fb3bf
rjzap <rick.j.zappala@gmail.com>
2017-07-10 19:29:39 -0400

    set up groceries project

---
+++ groceries.py
@@ -0,0 +1,72 @@
+import code
+
+products = [
+    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
+    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
+    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
+    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
+    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
+    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
+    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
+    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
+    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
+    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
+    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
+    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
+    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
+    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
+    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
+    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
+    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
+    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
+    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
+    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
+]
+
+##section 1-printing products and prices
+
+print "---------------"
+print "THERE ARE",len(products), "PRODUCTS"
+
+#for p in products:
+#    print "+",p["name"],"($",p["price"],")"
+
+def prod_name(products):
+    return products["name"]
+
+prod_sort = sorted(products,key=prod_name)
+
+#print prod_sort
+
+for f in prod_sort:
+    print "+",f["name"],"($",f["price"],")"
+
+
+##section 2 output departments and product count
+print "---------------"
+
+from itertools import groupby
+from operator import itemgetter
+
+#def prod_dep(products):
+    #return products["department"]
+
+#dep_sort = sorted(products,key=prod_dep)
+
+#for d in dep_sort:
+#    print "+", d["department"],
+
+prod_dep = sorted(products, key=itemgetter('department'))
+
+for k,v in itertools.groupby(products, key=itemgetter('department')):
+    print
+
+
+
+print "THERE ARE"
+
+for d in products:
+    print "+", d["department"]
+
+
+# code.interact(local=locals())
